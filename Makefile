e523ca5ffa4040fd29bf62f6ceab87f1bc2f9cb5
jlxip <jlxip@protonmail.com>
2022-07-03 08:43:43 +0200

    Very simple

---
+++ Makefile
@@ -0,0 +1,11 @@
+export PROJNAME := ramblock
+export RESULT := ramblock
+
+.PHONY: all
+
+all: $(RESULT)
+	@
+
+%: force
+	@$(MAKE) -f $(STRIFE_HELPER)/Makefile $@ --no-print-directory
+force: ;
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <shared_memory>
+#include <rpc>
+#include <unordered_map>
+#include <mutex>
+
+bool connect(std::PID client, std::SMID smid) {
+	return std::sm::connect(client, smid);
+}
+
+static std::unordered_map<uint64_t, uint8_t*> blocks;
+static std::mutex lock;
+
+bool read(std::PID client, uint64_t lba) {
+	uint8_t* buffer = std::sm::get(client);
+	if(!buffer)
+		return false;
+
+	lock.acquire();
+	if(blocks.find(lba) == blocks.end()) {
+		lock.release();
+		memset(buffer, 0, PAGE_SIZE);
+	} else {
+		memcpy(buffer, blocks[lba], PAGE_SIZE);
+		lock.release();
+	}
+	return true;
+}
+
+bool write(std::PID client, uint64_t lba) {
+	uint8_t* buffer = std::sm::get(client);
+	if(!buffer)
+		return false;
+
+	lock.acquire();
+	if(blocks.find(lba) == blocks.end())
+		blocks[lba] = (uint8_t*)std::mmap();
+
+	memcpy(blocks[lba], buffer, PAGE_SIZE);
+	lock.release();
+	return true;
+}
+
+extern "C" void _start() {
+	std::exportProcedure((void*)connect, 1);
+	std::exportProcedure((void*)read, 1);
+	std::exportProcedure((void*)write, 1);
+	std::enableRPC();
+	std::publish("ramblock");
+	std::halt();
+}
